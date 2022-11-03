1889131a5b0677621bfffdd996a1cb5e58cfda20
Fuentes Cristian <110264073+Fuentes-Cristian@users.noreply.github.com>
2022-11-03 16:07:45 -0300

    agregue js a mi proyecto casi terminado

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const reservarTurno = document.getElementById ("enviar")
 data = document.getElementById("table")
-vaciarFormulario = document.getElementById("form");
+vaciarFormulario = document.getElementById("form")
+devoluciones = document.getElementById("devoluciones")
+datosProductos = document.getElementById("productos");
 
 function Turno (nombre, fecha) {
    this.nombre = nombre;
@@ -35,10 +37,41 @@ function guardarDatos() {
 
 };
 
+const JSON = {
+   Corte:"$1.000",
+   Tintura:"$2.500",
+   Alisado:"$3.000",
+   Peinado:"$3.500"
+}
 
+//console.log(JSON);
+function fetch1() {
+   fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(datos =>{
+  // console.log(datos);
+   datos = JSON;
+  // console.log(datos);
+   const productos1 = document.createElement("productos1")
+   //console.log(productos);
+   JSON.innerHTML = productos;
+   //console.log(JSON);
+   productos.innerHTML = `
+   <div class="input-group mb-3">
+          <label class="input-group-text" for="inputGroupSelect01">Selecciona</label>
+       <select class="form-select" id="inputGroupSelect01">
+               <option selected>Ninguna</option>
+               <option selected>Corte</option>
+               <option value="1">Tintura</option>
+               <option value="2">Peinado</option>
+               <option value="3">Alisado</option>
+       </select>
+   </div> `
 
+      productos.appendChild(productos1)
+})
+}
 
-
-
+fetch1()
 //min  1.33
 

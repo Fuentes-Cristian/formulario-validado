c8212f3976b93a384dab749b233b60b56ffe68f1
Fuentes Cristian <110264073+Fuentes-Cristian@users.noreply.github.com>
2022-11-05 17:55:56 -0300

    agregue libreria a mi proyecto

--- js/main.js
+++ js/main.js
@@ -15,11 +15,19 @@ reservarTurno.addEventListener("click", (e) =>{
    e.preventDefault();
    let nombreGuardar = document.getElementById("nombre").value;
    let fechaGuardar = document.getElementById("fecha").value;
-   let nuevaSeleccion =  document.getElementById("modos").value;
+   let nuevaSeleccion =  document.getElementById("select").value;
      
    nuevoTurno = new Turno(nombreGuardar, fechaGuardar, nuevaSeleccion);
 
    guardarDatos();
+    //sweetAlert
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Turno reservado',
+      showConfirmButton: false,
+      timer: 1500
+    })
    vaciarFormulario.reset()
 });
 
@@ -33,43 +41,26 @@ function guardarDatos() {
    const datosDom = document.getElementById("tabla-turnos");
 
    datosDom.innerHTML += '<td>'+nuevoTurno.nombre+'</td><td>'+nuevoTurno.fecha+'</td><td>'+nuevoTurno.nuevaSeleccion+'</td>';
-
+   
    baseDatosCadena = JSON.stringify (baseDatos);
    localStorage.setItem ("MisTurnos", baseDatosCadena);
 
 };
 
-const JSON1 = {
-   Corte:"$1.000",
-   Tintura:"$2.500",
-   Alisado:"$3.000",
-   Peinado:"$3.500"
-}
-
-//console.log(JSON);
 function fetch1() {
-   fetch('https://jsonplaceholder.typicode.com/posts')
+   fetch('./js/datos.json')
   .then(response => response.json())
   .then(datos =>{
-   datos = JSON1;
-   const seleccionaModo = document.createElement("productos");
-   JSON1.innerHTML = seleccionaModo;
-   seleccionaModo.innerHTML = `
-   <div class="input-group mb-3">
-          <label class="input-group-text" for="inputGroupSelect01">Selecciona</label>
-       <select class="form-select" id="modos">
-               <option selected>Ninguna</option>
-               <option selected>Corte</option>
-               <option selected>Tintura</option>
-               <option selected>Peinado</option>
-               <option selected>Alisado</option>
-       </select>
-   </div> `
-
-      datosProductos.appendChild(seleccionaModo);
+   
+   datos.forEach(e => {
+      const seleccionaModo = document.createElement("option");
+      seleccionaModo.innerHTML = `<option value="${e.servicios}">${e.servicios}</option>`;
+   
+      const select = document.getElementById("select");
+      select.appendChild(seleccionaModo);
+   });
 })
 }
-
 fetch1()
 //min  1.33
 
